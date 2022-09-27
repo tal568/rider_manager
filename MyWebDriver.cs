05fa7e82134ad16e34628b9242836577384fae7b
John Doe <johndoe@example.com>
2022-09-27 21:36:24 +0300

    Add project files.

---
+++ MyWebDriver.cs
@@ -0,0 +1,78 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace whatapp_ride_joiner
+{
+
+    class MyWebDriver
+    {
+        IWebDriver driver;
+        public MyWebDriver()
+        {
+            driver = new ChromeDriver(@"C:\My Projects");
+            driver.Url = "https://web.whatsapp.com";
+            string btn_start_xpath = "//button[@aria-label='Search or start new chat']";
+            Console.WriteLine("scan QR code");
+            Console.ReadLine();
+            IWebElement start = driver.FindElement(By.XPath(btn_start_xpath));
+            start.Click();
+            Thread.Sleep(100);
+        }
+        public void Close()
+        {
+            this.driver.Quit();
+        }
+        public string GetPage()
+        {
+            return driver.PageSource;
+        }
+        public IList<IWebElement> FineElementsByXpath(string xpath)
+        {
+            return this.driver.FindElements(By.XPath(xpath));
+        }
+        public IWebElement FineElementByXpath(string xpath)
+        {
+            return this.driver.FindElement(By.XPath(xpath));
+        }
+        public void choose_group(string group)
+        {
+            string chat_xpath = $"//span[@title='{group}']";
+            IWebElement chat = driver.FindElement(By.XPath(chat_xpath));
+            chat.Click();
+        }
+        public IList<IWebElement> GetMassges()
+        {
+            DateTime today = DateTime.Today;
+            string format_today = today.ToString("yyyy");
+            string get_messages_by_xpath = $"//*[contains(@class,'message-in')]//div[@data-testid='msg-container']//*[contains(@data-pre-plain-text,'{format_today}')]";
+            Console.WriteLine("loading messages pres enter when done");
+            Console.ReadLine();
+            IList<IWebElement> messages = driver.FindElements(By.XPath(get_messages_by_xpath));
+
+            return messages;
+        }
+
+        public void Replay(IWebElement element)
+        {
+            string text = "אני";
+            Actions a = new Actions(driver);
+            a.MoveToElement(element).Perform();
+            string menue_xpath = "//div[@data-testid='icon-down-context']";
+            IWebElement menue = driver.FindElement(By.XPath(menue_xpath));
+            menue.Click();
+            string reply_xpath = "//div[@aria-label='Reply']";
+            IWebElement reply = driver.FindElement(By.XPath(reply_xpath));
+            reply.Click();
+            string inp_xpath = "//p[@class='selectable-text copyable-text']";
+            IWebElement input_box = driver.FindElement(By.XPath(inp_xpath));
+            input_box.SendKeys(text);
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.ML;
+
+namespace whatapp_ride_joiner
+{
+    public class ride_manger
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            MyWebDriver webdriver = new MyWebDriver();
+            webdriver.choose_group("אבא");
+            IList<IWebElement> messages = webdriver.GetMassges();
+            webdriver.Close();
+            //Load sample data
+            */
+            //Load sample data
+            var sampleData = new Ride_choser_model.ModelInput()
+            {
+                Col0 = @"מישהו יוצא ב17:00",
+            };
+
+            //Load model and predict output
+            var result = Ride_choser_model.Predict(sampleData);
+            for (int i=0; i<result.Score.Length;i++)
+             {
+                if (result.Score.Length - 1 != i)
+                {
+                    Console.WriteLine("for the value " + (i) + " the % is " + result.Score[i+1]);
+
+                }
+                else
+                {
+                    Console.WriteLine("for the value " + 3  + " the % is " + result.Score[0]);
+
+                }
+
+            }
+            Console.WriteLine("the predictedlabel is " + result.PredictedLabel);
+
+
+        }
+
+
+
+
+        public static void train_nlp(IList<IWebElement> messages)
+        {
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\tal\source\repos\whatapp_driver\messages.txt"))
+            {
+                foreach (IWebElement message in messages)
+                {
+                    writer.WriteLine(message.Text.Replace('\n', ' '));
+                }
+            }
+        }
+    }
+}
